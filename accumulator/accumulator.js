ab9b7d01f3795e93d29c8d3e234ea3d7540db02e
Neeraja37 <55628222+Neeraja37@users.noreply.github.com>
2022-01-31 16:41:49 +0530

    add

--- accumulator/accumulator.js
+++ accumulator/accumulator.js
@@ -1,1 +1,21 @@
+var accumulator = {
+   result: 0,
 
+   add: function (x) {
+      this.result += x;
+   },
+   subtract: function (x) {
+      this.result -= x;
+   },
+   multiply: function (x) {
+      this.result *= x;
+   },
+   divide: function (x) {
+      this.result /= x;
+   },
+   getResult: function () {
+      return this.result;
+   }
+}
+
+module.exports = accumulator;
